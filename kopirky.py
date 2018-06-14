e7221e671c99fd4aeb0892401e2cb84bc444cbf2
KristynaSop <ksopuskova@gmail.com>
2018-06-14 11:00:17 +0200

    Delete kopirky.py

--- kopirky.py
+++
@@ -1,6 +0,0 @@
-import shutil, os
-os.chdir("/Users/kristynasopuskova/Documents")
-shutil.copy("/Users/kristynasopuskova/Documents/spam.txt", "/Users/kristynasopuskova/Documents/delicious")
-shutil.copy("/Users/kristynasopuskova/Documents/eggs.txt","/Users/kristynasopuskova/Documents/delicious/eggs2.txt" )
-
-
