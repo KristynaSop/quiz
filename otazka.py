35dc4d80d06465207fd098f748bf3f1ef51dbe5b
KristynaSop <ksopuskova@gmail.com>
2018-06-14 11:02:29 +0200

    Delete otazka.py

--- otazka.py
+++
@@ -1,7 +0,0 @@
-otazka_1 = input("zadaj prve cislo")
-otazka_2 = input("zadaj druhe cislo")
-otazka_3 = input("zadaj tretie cislo")
-
-if otazka_1 < otazka_2 and otazka_1 < otazka_3:
-    print(otazka_1)
-    
